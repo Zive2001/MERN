8fdc134b51fac29be0c031a7f3636d11004ebeed
Supun Seneviratne <supunseneviratne@gmail.com>
2024-01-27 06:26:00 +0530

    Create Pages and Routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+       reprehenderit. Magnam animi ea explicabo in molestiae quas corrupti
+        voluptates consequatur numquam repellat dolore laboriosam odio quasi, odit possimus voluptate nihil.
+    </div>
+  )
+}
